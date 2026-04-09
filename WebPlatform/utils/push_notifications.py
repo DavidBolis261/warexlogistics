793df42dcd39838f8c762537daa2d5b5f3663a41
David Bolis <davidmaged1@hotmail.com>
2026-04-09 18:11:08 +1000

    soundeffectw

--- WebPlatform/utils/push_notifications.py
+++ WebPlatform/utils/push_notifications.py
@@ -109,7 +109,7 @@ def send_push_notification(device_token: str, title: str, body: str, data: dict 
                 "title": title,
                 "body": body,
             },
-            "sound": "default",
+            "sound": "notification.caf",
             "badge": 1,
         }
     }
